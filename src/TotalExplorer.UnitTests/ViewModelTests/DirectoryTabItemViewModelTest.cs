157541d19a27568f5e7ef05eb6140ab13c38dde3
Alexey <lesha29kartashev@yandex.ru>
2025-03-03 12:57:56 +0300

    test: add unit test

---
+++ src/TotalExplorer.UnitTests/ViewModelTests/DirectoryTabItemViewModelTest.cs
@@ -0,0 +1,24 @@
+﻿using TotalExplorer.ViewModels;
+using TotalExplorer.ViewModels.FileSystem;
+
+namespace TotalExplorer.UnitTests.ViewModelTests;
+
+public class DirectoryTabItemViewModelTest
+{
+    [Fact]
+    public void ChangeDirectory_DirectoryShoudBeChanged()
+    {
+        // Arrange
+        var tabItem = new DirectoryTabItemViewModel();
+        var newDirInfo = new DirectoryInfo("/Work");
+        string? curDirPath = tabItem.CurrentDirectory!.FullName;
+
+        // Act
+        tabItem.OpenDirectoryCommand.Execute(new DirectoryViewModel(newDirInfo)).Subscribe();
+
+        // Assert
+        Assert.NotEqual(curDirPath, tabItem.CurrentDirectory!.FullName);
+    }
+
+
+}
